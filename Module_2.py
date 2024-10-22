bc448e046e4169ad91564f15b48e17a57e75a93d
rrc_F2024_agomes62 <agomes62@academic.rrc.ca>
2024-10-22 17:03:25 -0500

    Experimented with built-in functions

--- Module_2.py
+++ Module_2.py
@@ -6,3 +6,33 @@ Usage: To demonstrate content from Module 2
 """
 
 #This is a single comment
+
+absolute_value=abs(-12)
+print('absolute value', absolute_value)
+
+# Function embedded within another function
+print('absolute value', abs(-12))
+
+print("Hello World!")
+
+print("I am", 25, "years old.")
+
+print("apples", "oranges", "bananas" , sep=",")
+
+name = "Anthony"
+age = 22
+print(f"My name is {name} and I am {age} years old.")
+# "f" at the front allows you to use the curly brackets. If you take the f away it will print the variable place holders instead of the variables themselves
+
+value = 3.14159
+print(f"the value is {value:.2f}")
+#.2f takes a number and rounds it to two decimal points. you have to add : after your variable if you want to add pieces to it. {value:.2f} GOOD. {value.2f} BAD
+# Also changing the number in ".2f" to ".4f" would have it round to 4 decimal points instead of 2.
+
+number = 12345
+print(f"The number is {number:05}")
+#it's very important that you add a 0 in front of whatever number you want to set it to. This trick doesn't seem to work with numbers with decimals
+
+name = "Anthony"
+print(f"Hello, {name:>10}.")
+# :>10 puts ten spaces between the comma and the name
